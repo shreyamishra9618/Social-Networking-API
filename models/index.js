217590f6ced4f77546b0d0fa00583fdf239281f1
shreyamishra9618 <shreyamishra9618@gmail.com>
2022-11-27 12:04:16 -0800

    added controllers, routes and util file for date formatting

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
-const Student = require('./Student');
-const Course = require('./Course');
+const Thought = require('./Thought');
+const User = require('./User');
 
-module.exports = { Student, Course };
+
+module.exports = {Thought, User};
