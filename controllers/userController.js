92470c2f777b76e43b83369f6d95bed5f9146569
shreyamishra9618 <shreyamishra9618@gmail.com>
2022-11-27 17:05:14 -0800

    updated and corrected the delete friend function

--- controllers/userController.js
+++ controllers/userController.js
@@ -59,6 +59,16 @@ const userController = {
         .catch(err => res.status(400).json(err));
     },
     deleteFriend({params}, res) {
+        User.findOneAndDelete({_id: params.id})
+        .then(userData => {
+            if(!userData) {
+                res.status(404).json({ message: 'No user found with this ID!' });
+                return;
+            }
+
+            res.json(userData);
+        })
+        .catch(err => res.status(400).json(err));
 
     }
 }
